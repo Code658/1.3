32b22692195c8e80caff09124ca3ce61f9abaf4d
Code658 <jkleinhenz7@gmail.com>
2020-11-18 08:22:08 -0500

    practicing get workflow

---
+++ SoftDevI/Unit14/assignment-14-code658/review01.py
@@ -0,0 +1,23 @@
+# E:\Users\Optimus\primes.txt
+# E:\Users\Optimus\hello.py
+# E:\Users\Program Files\Python\python.exe
+# E:\Users\Program Files\Git\git-bash.exe
+
+# cd Users\Optimus
+
+# cd ..
+
+# ls E:\Program Files
+
+# notepad info.txt
+
+# rm hello.py
+
+
+"""
+git status
+git add names.txt
+git commit -m "I am commiting!"
+git push
+git pull
+"""
