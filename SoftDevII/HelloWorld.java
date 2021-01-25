3b35f59022c4b534925673b5858cb59fb54499bd
Code658 <jkleinhenz7@gmail.com>
2021-01-25 13:57:04 -0500

    second class commit

--- SoftDevII/HelloWorld.java
+++ SoftDevII/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
+    public static void helloName(String first, String last) {
+        System.out.println("Hello, " + first + " " + last + "!");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        helloName("Rich", "Kleinhenz");
     }
 }
