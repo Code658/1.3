fe00e393957d431668e7ca40685957d82af55729
Code658 <jkleinhenz7@gmail.com>
2021-01-25 13:40:41 -0500

    first commit of the new semester

---
+++ SoftDevII/Primitives.java
@@ -0,0 +1,8 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int x = 5;
+        float y = 12.34f;
+
+        System.out.println("int x = " + x);
+    }
+}
---
+++ SoftDevII/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String test = "Paris";
+
+        System.out.println(test.charAt(0));
+        System.out.println(test.charAt(4));
+    }
+}
